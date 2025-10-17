c7e5c9e4303731b36d6c0614fcf23f5657fe820f
ankitklu <ankit.klu.2022@gmail.com>
2025-10-17 22:20:29 +0530

    feat: socketEvents initialized , initializeSocket | getIO

---
+++ Backend/src/socket/socketEvents.js
@@ -0,0 +1,47 @@
+const socketIo = require('socket.io');
+const socketEvents = require('./socketEvents');
+
+let io;
+
+
+const initializeSocket = (server) => {
+  io = socketIo(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"]
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('Player connected:', socket.id);
+
+    // Handle join queue
+    socket.on('joinQueue', (data) => {
+      socketEvents.handleJoinQueue(socket, io, data);
+    });
+
+    // Handle make move
+    socket.on('makeMove', (data) => {
+      socketEvents.handleMakeMove(socket, io, data);
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', () => {
+      socketEvents.handleDisconnect(socket, io);
+    });
+  });
+
+  return io;
+};
+
+const getIO = () => {
+  if (!io) {
+    throw new Error('Socket.IO not initialized');
+  }
+  return io;
+};
+
+module.exports = {
+  initializeSocket,
+  getIO
+};
